a0d733f3eda3f3f11bfb663e295b9f2cf3df0aa1
iammarkps <markkung08@gmail.com>
2018-12-11 23:13:17 +0700

    :sparkles: Add favicon and now.json

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,39 @@ export default () => (
         name="viewport"
         content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/favicons/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/static/manifest.json" />
+      <link
+        rel="mask-icon"
+        href="/static/favicons/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicons/32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicons/16x16.png"
+      />
+      <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+      <meta name="theme-color" color="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Montserrat:300,600,700|Kanit:400"
+        rel="stylesheet"
+      />
+      <title>Triam Udom Open House</title>
     </Head>
     <GlobalStyle />
     <Parallax pages={2}>
