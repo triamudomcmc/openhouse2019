3d13ab981c6fe75c12cea416015d9da5fe37a7e0
iammarkps <markkung08@gmail.com>
2018-12-25 14:22:32 +0700

    :sparkles: Add legal page!

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+const withMDX = require('@zeit/next-mdx')({
+  extension: /.mdx?$/
+})
+module.exports = withCSS(
+  withMDX({
+    pageExtensions: ['js', 'md', 'mdx']
+  })
+)
