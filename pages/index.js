8791ca9d48b7642de84b79a7997580cd9a90b40c
iammarkps <markkung08@gmail.com>
2019-01-04 23:10:33 +0700

    Improve UI

--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,32 @@
-import styled from 'styled-components'
 import React from 'react'
-import { beat, fontSize, Tracking, ViewType } from '../components/design'
-import { Parallax, ParallaxLayer } from 'react-spring/addons.cjs'
 
 import { enhance } from '../components/design/withViewType'
-import { XS_MOBILE, MOBILE } from '../components/design/withViewType'
 
 import { Layout } from '../components/layout'
 import { Title } from '../components/title'
 import { Booth } from '../components/booth'
+import { Contest } from '../components/contest'
 import { Schedule } from '../components/schedule'
 import { Register } from '../components/register'
 import { Map } from '../components/map'
 
-import 'nes.css/css/nes.min.css'
+import 'bulma/css/bulma.min.css'
+import '../static/css/nes.css'
 import '../static/css/index.css'
 import '../static/css/nprogress.css'
 
-const ScrollWrapper = styled.div`
-  width: 1;
-  height: ${beat(3)};
-  margin: ${beat(1)} auto;
-  color: black;
-  position: absolute;
-  text-align: center;
-  bottom: 0;
-  width: 100%;
-  letter-spacing: ${Tracking.wide};
-  font-size: ${fontSize(-3)};
-
-  ${ViewType.mobile} {
-    font-size: ${fontSize(-7)};
-  }
-`
-
-const Scroll = React.memo(props => (
-  <ParallaxLayer offset={0} speed={0.7}>
-    <ScrollWrapper>S C R O L L</ScrollWrapper>
-  </ParallaxLayer>
-))
-
-const StarBg = React.memo(props => (
-  <Parallax.Layer
-    offset={1}
-    speed={0}
-    factor={props.viewType === MOBILE ? 8 : 5}
-    style={{
-      backgroundImage: "url('/static/svg/star.svg')",
-      backgroundSize: 'cover'
-    }}
-  />
-))
-
 class Index extends React.PureComponent {
-  scrollToRegis = () => {
-    this.parallax.scrollTo(5)
-  }
   render() {
     const { viewType } = this.props
     return (
       <React.Fragment>
         <Layout>
-          <Parallax
-            pages={this.props.viewType === MOBILE ? 8 : 5}
-            config={{ tension: 120, friction: 14 }}
-            ref={ref => (this.parallax = ref)}
-          >
-            <Title viewType={viewType} />
-            <Scroll scrollToRegis={this.scrollToRegis} />
-            <StarBg viewType={viewType} />
-            <Booth viewType={viewType} />
-            <Schedule viewType={viewType} />
-            <Map viewType={viewType} />
-            <Register viewType={viewType} />
-          </Parallax>
+          <Title viewType={viewType} />
+          <Booth viewType={viewType} />
+          <Contest viewType={viewType} />
+          <Schedule viewType={viewType} />
+          <Map viewType={viewType} />
+          <Register viewType={viewType} />
         </Layout>
       </React.Fragment>
     )
