12833761453e65dc6838af131655d5adf0443c60
iammarkps <markkung08@gmail.com>
2019-01-04 23:12:27 +0700

    :lipstick: Improve REG page

--- components/register.js
+++ components/register.js
@@ -1,22 +1,18 @@
-/*eslint no-console: ["error", { allow: ["error"] }] */
 import styled from 'styled-components'
 import React from 'react'
-import { ParallaxLayer } from 'react-spring/addons.cjs'
 import QRCode from 'qrcode.react'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
+import { Transition, animated, config } from 'react-spring'
 
 import { api } from '../lib/api'
 
 import { Box } from './design/ui'
 
 import { TypographicContext } from './design'
-import { MOBILE } from './design/withViewType'
 
 const Parent = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
+  transform: translateY(0);
   display: flex;
   justify-content: center;
   width: 100%;
@@ -114,6 +110,7 @@ const RegForm = props => (
             className={
               errors.name && touched.name ? 'nes-input is-error' : 'nes-input'
             }
+            placeholder="Name"
             onChange={handleChange}
             value={values.name || ''}
           />
@@ -126,6 +123,7 @@ const RegForm = props => (
                 ? 'nes-input is-error'
                 : 'nes-input'
             }
+            placeholder="Lastname"
             onChange={handleChange}
             value={values.lastname || ''}
           />
@@ -136,6 +134,8 @@ const RegForm = props => (
             className={
               errors.email && touched.email ? 'nes-input is-error' : 'nes-input'
             }
+            type="email"
+            placeholder="Email"
             onChange={handleChange}
             value={values.email || ''}
           />
@@ -175,23 +175,52 @@ const RegForm = props => (
               onBlur={handleBlur}
             />
             <span>Teacher</span>
+          </label>{' '}
+          <label>
+            <input
+              name="type"
+              type="radio"
+              value="others"
+              checked={values.type === 'others'}
+              onChange={handleChange}
+              className="nes-radio"
+              onBlur={handleBlur}
+            />
+            <span>Others</span>
           </label>
           <br />
-          {values.type === 'student' && (
-            <div>
-              <label htmlFor="stdyear">Student&apos;s Year</label>
-              <input
-                name="stdyear"
-                className={
-                  errors.stdyear && touched.stdyear
-                    ? 'nes-input is-error'
-                    : 'nes-input'
-                }
-                onChange={handleChange}
-                value={values.stdyear || ''}
-              />
-            </div>
-          )}
+          <Transition
+            items={values.type === 'student'}
+            native
+            config={config.slow}
+            from={{ visibility: 'hidden', height: 0 }}
+            enter={{ visibility: 'visible', height: 'auto' }}
+            leave={{ visibility: 'hidden', height: 0 }}
+          >
+            {show =>
+              show &&
+              (props => (
+                <animated.div style={props}>
+                  <label htmlFor="stdyear">Student&apos;s Year</label>
+                  <br />
+                  <select>
+                    <option value="m1" onChange={handleChange}>
+                      M1
+                    </option>
+                    <option value="m2" onChange={handleChange}>
+                      M2
+                    </option>
+                    <option value="m3" onChange={handleChange}>
+                      M3
+                    </option>
+                    <option value="others" onChange={handleChange}>
+                      Others
+                    </option>
+                  </select>
+                </animated.div>
+              ))
+            }
+          </Transition>
           <br />
           <button
             className={
@@ -212,6 +241,7 @@ export class Register extends React.PureComponent {
   state = {
     key: ''
   }
+
   submitToFirebase = async (values, resetForm) => {
     try {
       const res = await api.post(
@@ -236,37 +266,33 @@ export class Register extends React.PureComponent {
   }
   render() {
     return (
-      <ParallaxLayer
-        offset={this.props.viewType === MOBILE ? 8 : 4}
-        factor={1}
-        speed={0.5}
-      >
-        <Parent>
-          <Box title="Register">
-            {this.state.key === '' ? (
-              <RegForm submitToFirebase={this.submitToFirebase} />
-            ) : (
-              <div>
-                <TypographicContext>
-                  <h3 id="regtxt">โปรดเก็บ QR Code ไว้ยืนยันตัวตนหน้างาน</h3>
-                  <QRCode value={this.state.key} />
-                  <p>โดยสามารถดาวน์โหลดสูจิบัตรได้ที่นี่</p>
-                </TypographicContext>
-                <button className="nes-btn">Download!</button>
-                <TypographicContext>
-                  <br />
-                  <a
-                    onClick={() => this.setState({ key: '' })}
-                    style={{ color: 'gray' }}
-                  >
-                    ลงทะเบียนอีกครั้ง
-                  </a>
-                </TypographicContext>
-              </div>
-            )}
-          </Box>
-        </Parent>
-      </ParallaxLayer>
+      <section className="section">
+        <div className="container">
+          <Parent>
+            <Box title="Register">
+              {this.state.key === '' ? (
+                <RegForm submitToFirebase={this.submitToFirebase} />
+              ) : (
+                <div>
+                  <TypographicContext>
+                    <p style={{ color: 'red' }}>
+                      โปรดเก็บ QR Code ไว้ยืนยันตัวตนหน้างาน
+                    </p>
+                    <QRCode value={this.state.key} />
+                    <br />
+                    <a
+                      onClick={() => this.setState({ key: '' })}
+                      style={{ color: 'grey' }}
+                    >
+                      ลงทะเบียนอีกครั้ง
+                    </a>
+                  </TypographicContext>
+                </div>
+              )}
+            </Box>
+          </Parent>
+        </div>
+      </section>
     )
   }
 }
