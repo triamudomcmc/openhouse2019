d326a8cc3bb83d74b63b2d9f8779ccf2b3893931
iammarkps <markkung08@gmail.com>
2019-01-04 23:10:56 +0700

    :lipstick: Improve TITLE UI

--- components/title.js
+++ components/title.js
@@ -1,50 +1,60 @@
 import styled from 'styled-components'
 import React from 'react'
-import { ParallaxLayer } from 'react-spring/addons.cjs'
 import { Spring, config, animated } from 'react-spring'
 import { beat, fontSize, ViewType } from '../components/design'
 
 import { MOBILE } from '../components/design/withViewType'
 
 const Header = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 100%;
-  text-align: center;
   padding-bottom: ${beat(1)};
-  font-size: ${fontSize(1)};
+  font-size: ${fontSize(7)};
+  text-align: center;
   ${ViewType.mobile} {
-    font-size: ${fontSize(-1)};
-  }
-  ${ViewType.xsMobile} {
-    font-size: ${fontSize(-1)};
+    font-size: ${fontSize(4)};
   }
 `
 
 export const Title = React.memo(props => (
-  <ParallaxLayer offset={0} speed={0.5}>
-    <Header>
-      {props.viewType === MOBILE ? (
-        <div>
-          <h1>Triam Udom Open House</h1>
-          <h1>2019</h1>
-        </div>
-      ) : (
-        <Spring
-          native
-          from={{ opacity: 0 }}
-          to={{ opacity: 1 }}
-          config={config.molasses}
-        >
-          {props => (
-            <animated.div style={props}>
+  <section className="hero is-fullheight">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <Header>
+          {props.viewType === MOBILE ? (
+            <div>
               <h1>Triam Udom Open House</h1>
               <h1>2019</h1>
-            </animated.div>
+              <button
+                className="nes-btn is-success title-button"
+                onClick={scrollToContent}
+                style={{ marginTop: '50px' }}
+              >
+                START!
+              </button>
+            </div>
+          ) : (
+            <Spring native from={{ opacity: 0 }} to={{ opacity: 1 }}>
+              {props => (
+                <animated.div style={props}>
+                  <h1>Triam Udom Open House</h1>
+                  <h1>2019</h1>
+                  <button
+                    className="nes-btn is-success title-button"
+                    onClick={scrollToContent}
+                  >
+                    START!
+                  </button>
+                </animated.div>
+              )}
+            </Spring>
           )}
-        </Spring>
-      )}
-    </Header>
-  </ParallaxLayer>
+        </Header>
+      </div>
+    </div>
+  </section>
 ))
+
+const scrollToContent = () => {
+  document
+    .getElementById('programs')
+    .scrollIntoView({ behavior: 'smooth', block: 'center' })
+}
