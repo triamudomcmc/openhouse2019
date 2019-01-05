3e6c38f59955c1e5470887a85177a4b04611921e
iammarkps <markkung08@gmail.com>
2019-01-05 18:50:33 +0700

    :ambulance:

--- components/register.js
+++ components/register.js
@@ -3,7 +3,6 @@ import React from 'react'
 import QRCode from '../lib/qr'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
-import { Transition, animated, config } from 'react-spring'
 
 import { api } from '../lib/api'
 
@@ -40,228 +39,220 @@ const SignupSchema = Yup.object().shape({
     .required('Required')
 })
 
-const RegForm = props => (
-  <Formik
-    initialValues={{
-      prefix: 'mister',
-      name: '',
-      lastname: '',
-      email: '',
-      type: 'student',
-      stdyear: ' '
-    }}
-    validationSchema={SignupSchema}
-    onSubmit={(values, { resetForm }) => {
-      props.submitToFirebase(values, resetForm)
-    }}
-  >
-    {({
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting
-    }) => (
-      <div
-        className="nes-field"
-        style={{
-          fontFamily: 'Noto Sans Thai UI, Noto Sans',
-          fontWeight: '600'
-        }}
-      >
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="prefix">คำนำหน้าชื่อ</label> <br />
-          <label>
+const RegForm = props => {
+  return (
+    <Formik
+      initialValues={{
+        prefix: 'mister',
+        name: '',
+        lastname: '',
+        email: '',
+        type: 'student',
+        stdyear: ''
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values, { resetForm }) => {
+        props.submitToFirebase(values, resetForm)
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <div
+          className="nes-field"
+          style={{
+            fontFamily: 'Noto Sans Thai UI, Noto Sans',
+            fontWeight: '600'
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="prefix">คำนำหน้าชื่อ</label> <br />
+            <label>
+              <input
+                name="prefix"
+                type="radio"
+                value="mister"
+                checked={values.prefix === 'mister'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>นาย</span>
+            </label>{' '}
+            <label>
+              <input
+                name="prefix"
+                type="radio"
+                value="miss"
+                checked={values.prefix === 'miss'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>นางสาว</span>
+            </label>{' '}
+            <label>
+              <input
+                name="prefix"
+                type="radio"
+                value="mrs"
+                checked={values.prefix === 'mrs'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>นาง</span>
+            </label>
+            <label>
+              <input
+                name="prefix"
+                type="radio"
+                value="master"
+                checked={values.prefix === 'master'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>เด็กชาย</span>
+            </label>
+            <label>
+              <input
+                name="prefix"
+                type="radio"
+                value="miss(child)"
+                checked={values.prefix === 'miss(child)'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>เด็กหญิง</span>
+            </label>
+            <br />
+            <label htmlFor="name">ชื่อ</label>
             <input
-              name="prefix"
-              type="radio"
-              value="mister"
-              checked={values.prefix === 'mister'}
+              name="name"
+              className={
+                errors.name && touched.name ? 'nes-input is-error' : 'nes-input'
+              }
+              placeholder="Name"
               onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
+              value={values.name || ''}
             />
-            <span>นาย</span>
-          </label>{' '}
-          <label>
+            <br />
+            <label htmlFor="lastname">นามสกุล</label>
             <input
-              name="prefix"
-              type="radio"
-              value="miss"
-              checked={values.prefix === 'miss'}
+              name="lastname"
+              className={
+                errors.lastname && touched.lastname
+                  ? 'nes-input is-error'
+                  : 'nes-input'
+              }
+              placeholder="Lastname"
               onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
+              value={values.lastname || ''}
             />
-            <span>นางสาว</span>
-          </label>{' '}
-          <label>
+            <br />
+            <label htmlFor="email">อีเมล</label>
             <input
-              name="prefix"
-              type="radio"
-              value="mrs"
-              checked={values.prefix === 'mrs'}
+              name="email"
+              className={
+                errors.email && touched.email
+                  ? 'nes-input is-error'
+                  : 'nes-input'
+              }
+              type="email"
+              placeholder="Email"
               onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
+              value={values.email || ''}
             />
-            <span>นาง</span>
-          </label>
-          <label>
-            <input
-              name="prefix"
-              type="radio"
-              value="master"
-              checked={values.prefix === 'master'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>เด็กชาย</span>
-          </label>
-          <label>
-            <input
-              name="prefix"
-              type="radio"
-              value="miss(child)"
-              checked={values.prefix === 'miss(child)'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>เด็กหญิง</span>
-          </label>
-          <br />
-          <label htmlFor="name">ชื่อ</label>
-          <input
-            name="name"
-            className={
-              errors.name && touched.name ? 'nes-input is-error' : 'nes-input'
-            }
-            placeholder="Name"
-            onChange={handleChange}
-            value={values.name || ''}
-          />
-          <br />
-          <label htmlFor="lastname">นามสกุล</label>
-          <input
-            name="lastname"
-            className={
-              errors.lastname && touched.lastname
-                ? 'nes-input is-error'
-                : 'nes-input'
-            }
-            placeholder="Lastname"
-            onChange={handleChange}
-            value={values.lastname || ''}
-          />
-          <br />
-          <label htmlFor="email">อีเมล</label>
-          <input
-            name="email"
-            className={
-              errors.email && touched.email ? 'nes-input is-error' : 'nes-input'
-            }
-            type="email"
-            placeholder="Email"
-            onChange={handleChange}
-            value={values.email || ''}
-          />
-          <br />
-          <label>
-            <input
-              name="type"
-              type="radio"
-              value="student"
-              checked={values.type === 'student'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>นักเรียน</span>
-          </label>{' '}
-          <label>
-            <input
-              name="type"
-              type="radio"
-              value="parent"
-              checked={values.type === 'parent'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>ผู้ปกครอง</span>
-          </label>{' '}
-          <label>
-            <input
-              name="type"
-              type="radio"
-              value="teacher"
-              checked={values.type === 'teacher'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>ครู/อาจารย์</span>
-          </label>{' '}
-          <label>
-            <input
-              name="type"
-              type="radio"
-              value="others"
-              checked={values.type === 'others'}
-              onChange={handleChange}
-              className="nes-radio"
-              onBlur={handleBlur}
-            />
-            <span>อื่น ๆ</span>
-          </label>
-          <br />
-          <Transition
-            items={values.type === 'student'}
-            native
-            config={config.slow}
-            from={{ visibility: 'hidden', height: 0 }}
-            enter={{ visibility: 'visible', height: 'auto' }}
-            leave={{ visibility: 'hidden', height: 0 }}
-          >
-            {show =>
-              show &&
-              (props => (
-                <animated.div style={props}>
-                  <label htmlFor="stdyear">ระดับการศึกษา</label>
-                  <br />
-                  <select name="stdyear" onChange={handleChange}>
-                    <option value="p1-3">ประถมศึกษาตอนต้น</option>
-                    <option value="p4-6">ประถมศึกษาตอนปลาย</option>
-                    <option value="m1">มัธยมศึกษาปีที่ 1</option>
-                    <option value="m2">มัธยมศึกษาปีที่ 2</option>
-                    <option value="m3">มัธยมศึกษาปีที่ 3</option>
-                    <option value="highschool">มัธยมศึกษาตอนปลาย</option>
-                    <option value="others">อื่น ๆ</option>
-                  </select>
-                </animated.div>
-              ))
-            }
-          </Transition>
-          <br />
-          <button
-            className={
-              isSubmitting ? 'nes-btn is-disabled' : 'nes-btn is-primary'
-            }
-            type="submit"
-            disabled={isSubmitting}
-            style={{ fontWeight: '700' }}
-          >
-            ลงทะเบียน
-          </button>
-        </form>
-      </div>
-    )}
-  </Formik>
-)
+            <br />
+            <label>
+              <input
+                name="type"
+                type="radio"
+                value="student"
+                checked={values.type === 'student'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>นักเรียน</span>
+            </label>{' '}
+            <label>
+              <input
+                name="type"
+                type="radio"
+                value="parent"
+                checked={values.type === 'parent'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>ผู้ปกครอง</span>
+            </label>{' '}
+            <label>
+              <input
+                name="type"
+                type="radio"
+                value="teacher"
+                checked={values.type === 'teacher'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>ครู/อาจารย์</span>
+            </label>{' '}
+            <label>
+              <input
+                name="type"
+                type="radio"
+                value="others"
+                checked={values.type === 'others'}
+                onChange={handleChange}
+                className="nes-radio"
+                onBlur={handleBlur}
+              />
+              <span>อื่น ๆ</span>
+            </label>
+            <br />
+            {values.type === 'student' && (
+              <React.Fragment>
+                <label htmlFor="stdyear">ระดับการศึกษา</label>
+                <br />
+                <select name="stdyear" onChange={handleChange}>
+                  <option value="p1-3">ประถมศึกษาตอนต้น</option>
+                  <option value="p4-6">ประถมศึกษาตอนปลาย</option>
+                  <option value="m1">มัธยมศึกษาปีที่ 1</option>
+                  <option value="m2">มัธยมศึกษาปีที่ 2</option>
+                  <option value="m3">มัธยมศึกษาปีที่ 3</option>
+                  <option value="highschool">มัธยมศึกษาตอนปลาย</option>
+                  <option value="others">อื่น ๆ</option>
+                </select>
+              </React.Fragment>
+            )}
+            <br />
+            <button
+              className={
+                isSubmitting ? 'nes-btn is-disabled' : 'nes-btn is-primary'
+              }
+              type="submit"
+              disabled={isSubmitting}
+              style={{ fontWeight: '700' }}
+            >
+              ลงทะเบียน
+            </button>
+          </form>
+        </div>
+      )}
+    </Formik>
+  )
+}
 
 export class Register extends React.PureComponent {
   state = {
@@ -287,6 +278,7 @@ export class Register extends React.PureComponent {
       console.error(err)
     }
   }
+
   resetKey = () => {
     this.setState({ key: '' })
   }
@@ -313,7 +305,10 @@ export class Register extends React.PureComponent {
           <Parent>
             <Box title="Register">
               {this.state.key === '' ? (
-                <RegForm submitToFirebase={this.submitToFirebase} />
+                <RegForm
+                  submitToFirebase={this.submitToFirebase}
+                  handleDropdown={this.handleDropdown}
+                />
               ) : (
                 <div>
                   <TypographicContext>
