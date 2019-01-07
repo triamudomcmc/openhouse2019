62f1454f214686340696bcb0c157ed889be7a32a
iammarkps <markkung08@gmail.com>
2019-01-07 12:29:55 +0700

    :fire: Remove dead code

--- pages/index.js
+++ pages/index.js
@@ -15,22 +15,18 @@ import '../static/css/nes.css'
 import '../static/css/index.css'
 import '../static/css/nprogress.css'
 
-class Index extends React.PureComponent {
-  render() {
-    const { viewType } = this.props
-    return (
-      <React.Fragment>
-        <Layout>
-          <Title viewType={viewType} />
-          <Booth viewType={viewType} />
-          <Contest />
-          <Schedule />
-          <Direction />
-          <Register />
-        </Layout>
-      </React.Fragment>
-    )
-  }
-}
+const Index = React.memo(props => {
+  const { viewType } = props
+  return (
+    <Layout>
+      <Title viewType={viewType} />
+      <Booth viewType={viewType} />
+      <Contest />
+      <Schedule />
+      <Direction />
+      <Register />
+    </Layout>
+  )
+})
 
 export default enhance(Index)
