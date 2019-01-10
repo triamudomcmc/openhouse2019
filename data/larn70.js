ef87aa641ad00bf769d53902d031ec62bbb7c71f
iammarkps <markkung08@gmail.com>
2019-01-10 11:43:08 +0700

    Update schedule

--- data/larn70.js
+++ data/larn70.js
@@ -1,120 +1,127 @@
 export const larn70 = [
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '08:00',
-    endTime: '09:30',
+    startTime: '08:30',
+    endTime: '10:00',
     name: 'พิธีการ',
     by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '09:30',
-    endTime: '09:40',
-    name: 'พิธีกรแนะนำงาน',
-    by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
+    startTime: '10:00',
+    endTime: '10:10',
+    name: 'การแสดงมโนราห์',
+    by: 'โรงเรียนเตรียมอุดมศึกษาภาคใต้'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '09:40',
-    endTime: '09:50',
-    name: 'Entertainer Showcase',
-    by: 'ชมรมสันทนากร'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '09:50',
-    endTime: '10:00',
+    startTime: '10:10',
+    endTime: '10:20',
     name: 'การแสดงคทากร',
     by: 'คณะคทากร'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '10:00',
-    endTime: '10:10',
+    startTime: '10:20',
+    endTime: '10:30',
     name: 'การแสดง มู่หลาน',
     by: 'ชมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาจีน'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '10:10',
-    endTime: '10:15',
-    name: 'การแสดงควงธง',
-    by: 'คัลเลอร์การ์ดสีแสด'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '10:15',
-    endTime: '10:30',
-    name: 'K-pop show',
-    by: 'ชมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาเกาหลี'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
     startTime: '10:30',
-    endTime: '10:45',
-    name: 'กิจกรรมสัมภาษณ์รุ่นพี่',
-    by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '10:45',
-    endTime: '10:50',
-    name: 'การแสดงเชียร์เพลงพระราชนิพนธ์',
-    by: 'ชมรมนันทนาการกีฬา'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '10:50',
-    endTime: '10:55',
-    name: 'การแสดงควงธง',
-    by: 'คัลเลอร์การ์ดสีบานเย็น'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '10:55',
-    endTime: '11:05',
-    name: 'การแสดง Soran',
-    by: 'ขมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาญี่ปุ่น'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '11:05',
-    endTime: '11:10',
-    name: 'การแสดงเชียร์เพลงเราเตรียม',
-    by: 'ชมรมนันทนาการกีฬา'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '11:10',
-    endTime: '11:20',
-    name: 'กิจกรรมแนะนำชมรม',
-    by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '11:20',
-    endTime: '11:25',
-    name: 'การแสดงควงธง',
-    by: 'คัลเลอร์การ์ดสีเขียว'
-  },
-  {
-    date: 'Fri, 11 Jan 2019',
-    startTime: '11:25',
-    endTime: '11:35',
+    endTime: '10:40',
     name: 'Entertainer Showcase',
     by: 'ชมรมสันทนากร'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '11:35',
-    endTime: '11:45',
+    startTime: '10:40',
+    endTime: '10:45',
+    name: 'การแสดงควงธง',
+    by: 'คัลเลอร์การ์ดสีแสด'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '10:45',
+    endTime: '11:00',
+    name: 'K-pop show',
+    by: 'ชมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาเกาหลี'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:00',
+    endTime: '11:15',
+    name: 'กิจกรรมสัมภาษณ์รุ่นพี่',
+    by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:15',
+    endTime: '11:25',
+    name: 'การแสดงเชียร์เพลงพระราชนิพนธ์',
+    by: 'ชมรมนันทนาการกีฬา'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:25',
+    endTime: '11:30',
+    name: 'การแสดงควงธง',
+    by: 'คัลเลอร์การ์ดสีบานเย็น'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:30',
+    endTime: '11:40',
+    name: 'การแสดง Soran',
+    by: 'ขมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาญี่ปุ่น'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:40',
+    endTime: '11:50',
+    name: 'การแสดงเชียร์เพลงเราเตรียม',
+    by: 'ชมรมนันทนาการกีฬา'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '11:50',
+    endTime: '12:00',
+    name: 'กิจกรรมแนะนำชมรม',
+    by: 'คณะกรรมการกลางงานกิจกรรมพัฒนาผู้เรียน'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '12:00',
+    endTime: '12:05',
+    name: 'การแสดงควงธง',
+    by: 'คัลเลอร์การ์ดสีเขียว'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '12:05',
+    endTime: '12:10',
+    name: 'การแสดงมโนราห์',
+    by: 'โรงเรียนเตรียมอุดมศึกษาภาคใต้'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '12:10',
+    endTime: '12:20',
+    name: 'Entertainer Showcase',
+    by: 'ชมรมสันทนากร'
+  },
+  {
+    date: 'Fri, 11 Jan 2019',
+    startTime: '12:20',
+    endTime: '12:30',
     name: 'Latin Dance',
     by: 'ชมรมสีสรรพ์ภาษาต่างประเทศที่ 2 ภาษาสเปน'
   },
   {
     date: 'Fri, 11 Jan 2019',
-    startTime: '11:45',
-    endTime: '14:00',
+    startTime: '12:30',
+    endTime: '14:30',
     name: 'การแสดงจากชมรมดนตรีสากล',
     by: 'ชมรมดนตรีสากล'
   },
@@ -188,6 +195,13 @@ export const larn70 = [
     endTime: '11:45',
     name: 'การแสดงควงธง',
     by: 'คัลเลอร์การ์ดสีฟ้า'
+  },
+  {
+    date: 'Sat, 12 Jan 2019',
+    startTime: '11:45',
+    endTime: '11:55',
+    name: 'French Chorus',
+    by: 'French Chorus'
   },
   {
     date: 'Sat, 12 Jan 2019',
